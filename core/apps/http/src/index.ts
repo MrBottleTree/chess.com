11e7c3764947a0474db52d72646c8dd6745327a3
Vishrut Ramraj <65703492+MrBottleTree@users.noreply.github.com>
2026-01-20 09:43:24 +0000

    HTTP backend successfully posts

--- core/apps/http/src/index.ts
+++ core/apps/http/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-const app = express()
-const port = 3000
+
+const app = express();
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello from chess.com repo!! lol');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
